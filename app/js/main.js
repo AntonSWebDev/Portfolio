59af3ce6972421b541becdca2dcfd3049913488b
Anton <elizarovanton35@gmail.com>
2023-01-06 10:55:51 +0200

    sad3

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,8 @@
 jQuery(document).ready(function($){
 
-	$('#header').vide('./../videos/header-video.mp4', {
+	$('#header').vide('../videos/header-video.mp4', {
 		posterType: 'detect',
 		bgColor: '#50627E'
-	});
-
-	$('.modal-btn').click(function(e) {
-		e.preventDefault();
-		$('body').addClass('lock');
-		var destinationPopup = $(this).attr("href");
-		$(destinationPopup).addClass('show');
-		$(destinationPopup + ' .popup__close').click(function() {
-			$(destinationPopup).removeClass('show');
-			$('body').removeClass('lock');
-		});
-		$(destinationPopup + ' .popup__body').click(function(e) {
-			e.stopPropagation();
-		});
-		$(destinationPopup + ' .popup__overlay').click(function() {
-			$(destinationPopup).removeClass('show');
-			$('body').removeClass('lock');
-		});
-		
 	});
   
 });
