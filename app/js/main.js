d8ea1f660a13ed1c679a502cc5c8aaf673d4468c
Anton <elizarovanton35@gmail.com>
2023-01-06 19:06:15 +0200

    sad123123

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function($){
+$(window).on('load', function() {
 
-	$('#header').vide('./../videos/header-video.mp4', {
+	$('#header').vide('videos/header-video', {
 		posterType: 'detect',
 		bgColor: '#50627E'
 	});
