8cf550dddf05cd5dcad595606775152179b4993a
Anton <elizarovanton35@gmail.com>
2023-01-30 03:55:55 +0200

    2p

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
-$(window).on('load', function() {
+$(window).on('load', function () {
 
-	$('#header').vide('videos/header-video', {
-		posterType: 'detect',
-		bgColor: '#50627E'
-	});
-  
+  $('#header').vide('videos/header-video', {
+    posterType: 'detect',
+    bgColor: '#50627E'
+  });
+
 });
